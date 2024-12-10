eb5176174bd8b6ca87e1190a6dbe1fa5a80c3abb
jdu211171 <211171m@jdu.uz>
2024-12-10 08:09:26 +0500

    delete

--- app/root.tsx
+++ app/root.tsx
@@ -1,90 +1,96 @@
-import { LinksFunction } from "@remix-run/node";
+import {LinksFunction, redirect} from "@remix-run/node";
 import {
-  Form, Link,
-  Links,
-  Meta, Outlet,
-  Scripts,
-  ScrollRestoration, useLoaderData
+    Form, Link,
+    Links,
+    Meta, NavLink, Outlet,
+    Scripts,
+    ScrollRestoration, useLoaderData, useNavigation
 } from "@remix-run/react";
 import appStylesHref from "./app.css?url";
-import { createEmptyContact, getContacts } from "./data";
+import {createEmptyContact, getContacts} from "./data";
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: appStylesHref },
+    {rel: "stylesheet", href: appStylesHref},
 ];
 
 export const action = async () => {
-  const contact = createEmptyContact();
-    return { contact };
+    const contact = await createEmptyContact();
+    return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export const loader = async () => {
-  const contacts = await getContacts();
-  return { contacts };
+    const contacts = await getContacts();
+    return {contacts};
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
-  return (
-    <html lang="en">
-      <head>
-        <title>My Remix App</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
+    const {contacts} = useLoaderData<typeof loader>();
+    const navigation = useNavigation();
+    return (
+        <html lang="en">
+        <head>
+            <title>My Remix App</title>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <Meta/>
+            <Links/>
+        </head>
+        <body>
         <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            {contacts.length ? (
-                <ul>
-                  {contacts.map((contact) => (
-                      <li key={contact.id}>
-                        <Link to={`contacts/${contact.id}`}>
-                          {contact.first || contact.last ? (
-                              <>
-                                {contact.first} {contact.last}
-                              </>
-                          ) : (
-                              <i>No Name</i>
-                          )}{" "}
-                          {contact.favorite ? (
-                              <span>★</span>
-                          ) : null}
-                        </Link>
-                      </li>
-                  ))}
-                </ul>
-            ) : (
-                <p>
-                  <i>No contacts</i>
-                </p>
-            )}
-          </nav>
+            <h1>Remix Contacts</h1>
+            <div>
+                <Form id="search-form" role="search">
+                    <input
+                        id="q"
+                        aria-label="Search contacts"
+                        placeholder="Search"
+                        type="search"
+                        name="q"
+                    />
+                    <div id="search-spinner" aria-hidden hidden={true}/>
+                </Form>
+                <Form method="post">
+                    <button type="submit">New</button>
+                </Form>
+            </div>
+            <nav>
+                {contacts.length ? (
+                    <ul>
+                        {contacts.map((contact) => (
+                            <li key={contact.id}>
+                                <NavLink to={`contacts/${contact.id}`}
+                                         className={({isActive, isPending}) => isActive
+                                             ? "active"
+                                             : isPending
+                                                 ? "pending"
+                                                 : ""}>
+                                    {contact.first || contact.last ? (
+                                        <>
+                                            {contact.first} {contact.last}
+                                        </>
+                                    ) : (
+                                        <i>No Name</i>
+                                    )}{" "}
+                                    {contact.favorite ? (
+                                        <span>★</span>
+                                    ) : null}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>
+                        <i>No contacts</i>
+                    </p>
+                )}
+            </nav>
         </div>
-        <div id="detail">
-          <Outlet />
+        <div className={navigation.state === "loading" ? "loading" : ""} id="detail">
+            <Outlet/>
         </div>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+        <ScrollRestoration/>
+        <Scripts/>
+        </body>
+        </html>
+    );
 }
